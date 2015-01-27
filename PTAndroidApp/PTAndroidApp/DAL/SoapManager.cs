8d36545efecefb3bc7687920a5914fbe3c15f10f
markabram20 <silvanasophia01>
2015-01-26 19:51:32 -0800

    add & delete drughx

--- PTAndroidApp/PTAndroidApp/DAL/SoapManager.cs
+++ PTAndroidApp/PTAndroidApp/DAL/SoapManager.cs
@@ -8,7 +8,7 @@ namespace PTAndroidApp
 {
 	public class SoapManager {
 		// ADD Patient: Requires new Patient Model without Patient Id
-		string clientUrl = "http://ptprojectapi.azurewebsites.net";
+		private static string clientUrl = "http://ptprojectapi.azurewebsites.net";
 		//string clientUrl = "https://localhost:44301";
 		//string clientUrl = "localhost:57131";
 
@@ -132,6 +132,56 @@ namespace PTAndroidApp
 			});
 
 			return true;
+		}
+
+		public DrugHistory AddDrugHistory(DrugHistory DrugHx){
+			// Put code to communicate to web service here
+			var client = new RestClient (clientUrl);
+			var request = new RestRequest("api/DrugHistories", Method.POST );
+
+			// serialize object to pass in request
+			string json = JsonConvert.SerializeObject (
+				DrugHx, 
+				new JsonSerializerSettings (){ 
+					DateFormatHandling = DateFormatHandling.IsoDateFormat,
+					NullValueHandling = NullValueHandling.Include 
+				});
+
+			// add request parameters
+			request.AddParameter("application/json; charset=utf-8", json, ParameterType.RequestBody);
+			request.RequestFormat = DataFormat.Json;
+
+			var response = Execute<DrugHistory> (request);
+
+			return response;
+		}
+
+		public DrugHistory DeleteDrugHistory(int rowId){
+			// Put code to communicate to web service here
+			var client = new RestClient (clientUrl);
+			var request = new RestRequest("api/DrugHistories/{id}", Method.DELETE );
+
+			// add request parameters
+			request.AddUrlSegment ("id", rowId.ToString ());
+
+			var response =  Execute<DrugHistory> (request);
+
+			return response;
+		}
+
+		static T Execute<T>(RestRequest request) where T: new()
+		{
+			var client = new RestClient(clientUrl);
+			var response = client.Execute<T>(request);
+
+			if (response.ErrorException != null)
+			{
+				const string message = "Error retrieving response.  Check inner details for more info.";
+				var requestException = new ApplicationException(message, response.ErrorException);
+				throw requestException;
+			}
+
+			return response.Data;
 		}
 	}
 }
